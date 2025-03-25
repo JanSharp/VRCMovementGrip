e4b0444ae7fe2a6bfaa20e3d8b5870d4863d42d2
JanSharp <steinmeister36@gmail.com>
2025-03-26 00:10:42 +0100

    Add test script for the move api function

---
+++ Runtime/Dev/MovementGripAPITest.cs
@@ -0,0 +1,21 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class MovementGripAPITest : UdonSharpBehaviour
+    {
+        public MovementGrip movementGrip;
+
+        public override void Interact()
+        {
+            movementGrip.SetLocalPositionOfToMove(movementGrip.toMove.localPosition + new Vector3(
+                Random.Range(-1f, 1f),
+                Random.Range(-1f, 1f),
+                Random.Range(-1f, 1f)));
+        }
+    }
+}
